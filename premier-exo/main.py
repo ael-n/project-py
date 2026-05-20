e4976c3a329cd7744df6a384dbfc8490ba821e31
ael-n <edwidgearie@gmail.com>
2026-05-20 12:06:17 -0400

    first

---
+++ premier-exo/main.py
@@ -0,0 +1,14 @@
+Créez une variable nommée nom et attribuez-y votre nom.
+Créez une variable nommée age et attribuez-y votre âge.
+Affichez une phrase contenant votre nom et votre âge en utilisant les variables que vous venez de créer. Par exemple, "Je m'appelle Jean et j'ai 25 ans." (Utilisez le f-string)
+Modifiez la valeur de la variable age pour ajouter 10 ans à votre âge actuel.
+Affichez à nouveau la phrase contenant votre nom et votre âge en utilisant les variables mises à jour. Par exemple, "Je m'appelle Jean et j'ai 35 ans maintenant." (Utilisez le f-string)
+
+
+nom = Arie
+age = 21
+
+print=(f"Je m'appelle {nom} jai {age} an.")
+
+age += 10
+print=(f"Je m'appelle {nom} jai {age} an.")
